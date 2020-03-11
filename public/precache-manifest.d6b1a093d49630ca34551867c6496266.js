95fc931a852639ba2a6368e51653b4bcd2d34b8a
turinglabsorg <turinglabs@criptext.com>
2020-03-11 14:58:39 +0100

    bootstrap nestjs

---
+++ public/precache-manifest.d6b1a093d49630ca34551867c6496266.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f71987b1e86c1f980e19a685acd040c",
+    "url": "/img/google-plus.3f71987b.svg"
+  },
+  {
+    "revision": "df4567b93f30c690d232f44e5771b254",
+    "url": "/scryptacore.js"
+  },
+  {
+    "revision": "bdfdaf898484cee047b9a6c9eb5fef58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eb129270dabe1100bcc",
+    "url": "/css/chunk-vendors.c99424ed.css"
+  },
+  {
+    "revision": "795ea9774c61434ac710408ba8626894",
+    "url": "/img/mail.795ea977.svg"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/img/github.1f33e75d.svg"
+  },
+  {
+    "revision": "cb3076432cc2796a0896aaca9737c3dc",
+    "url": "/img/iphone.cb307643.svg"
+  },
+  {
+    "revision": "380d97a671b3d01b218edddf720b4611",
+    "url": "/img/linkedin.380d97a6.svg"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/img/twitter.7e145c9e.svg"
+  },
+  {
+    "revision": "833fab81c8aa2f90a8cc",
+    "url": "/js/app.2072c169.js"
+  },
+  {
+    "revision": "9eb129270dabe1100bcc",
+    "url": "/js/chunk-vendors.947b45aa.js"
+  },
+  {
+    "revision": "ae4eac582f32032c09eb0d874581d372",
+    "url": "/fonts/stylesheet.css"
+  },
+  {
+    "revision": "9c64d453cda57d384a87b588f4e57bae",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "13c3aa2187a4740479874f67ef978ac1",
+    "url": "/fonts/karmilla-regular-webfont.woff"
+  },
+  {
+    "revision": "eeab8062bf99baf5f535829fefb72cab",
+    "url": "/fonts/karmilla-regular-webfont.woff2"
+  },
+  {
+    "revision": "173be8183292d3d2f615d10f4d0d46eb",
+    "url": "/fonts/karmilla-regular-webfont.ttf"
+  },
+  {
+    "revision": "833fab81c8aa2f90a8cc",
+    "url": "/css/app.c8d065a3.css"
+  }
+];
---
+++ ui/README.md
@@ -0,0 +1,3 @@
+# Scrypta News Feed
+
+This PoC will allow to share and read decentralized news feeds written on the Scrypta Blockchain.
