38f2141fce4ac96508908852582ecae323132d3a
turinglabsorg <turinglabs@criptext.com>
2020-09-23 10:18:40 +0200

    rss feed test

---
+++ src/router.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import New from './views/New.vue'
+import Login from './views/Login.vue'
+import News from './views/News.vue'
+import Author from './views/Author.vue'
+import Edit from './views/Edit.vue'
+import Gravatar from 'vue-gravatar'
+
+Vue.use(Router)
+Vue.component('v-gravatar', Gravatar);
+
+export default new Router({
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/new',
+      name: 'new',
+      component: New
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/news/:uuid',
+      name: 'News',
+      component: News
+    },
+    {
+      path: '/edit/:uuid',
+      name: 'Edit',
+      component: Edit
+    },
+    {
+      path: '/author/:address',
+      name: 'Author',
+      component: Author
+    }
+  ]
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="container" style="margin-top:40px">
+    <vue-headful
+      title="Scrypta Decentralized News Feed"
+      description="Read articles from trusted sources."
+    />
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1>Scrypta News</h1>
+        <h3>read articles from trusted sources.</h3>
+        <hr>
+        <div v-if="isLoading">Loading news from the blockchain...</div>
+        <div v-if="!isLoading">
+          <div v-for="news in feed" v-bind:key="news._id" class="feed" style="position:relative; float:none; overflow:hidden">
+            <div v-if="news.data !== 'upvote' && news.data !== 'downvote'" style="position:relative">
+              <a :href="'/#/news/' + news.uuid" style="position:absolute;bottom:40px; cursor:pointer; right:0;">
+                <b-icon-arrow-right class="arrow-dx"></b-icon-arrow-right>
+              </a>
+              <div v-if="news.data.image && news.data.image.indexOf('file://') === -1 && news.data.image.indexOf('http://') === -1 && news.data.image.indexOf('https://') === 0"><img style="margin-bottom:20px" :src="news.data.image" width="100%"></div>
+              <div v-if="!news.data.title">
+               <h3 style="margin:0; padding:0; font-size:17px!important; font-weight:bold; padding-right:40px">{{ news.refID }}</h3>
+              </div>
+              <div v-if="news.data.title">
+               <h3 style="margin:0; padding:0; font-size:17px!important; font-weight:bold; padding-right:40px">{{ news.data.title }}</h3>
+              </div>
+              <div v-if="news.data.tags">
+               <span v-for="tag in news.data.tags" v-bind:key="tag" style="margin:0; padding:0; font-size:13px; padding-right:10px">#{{ tag }}</span>
+              </div>
+              <div style="font-size:11px;" v-if="!news.data.creator">
+                Written by <b><a :href="'/#/author/' + news.address">{{ news.address.substr(0,3) }}...{{ news.address.substr(-3) }}</a></b> at block <i>{{ news.block }}</i>
+              </div>
+              <div style="font-size:11px;" v-if="news.data.creator">
+                Written by <b><a :href="'/#/author/' + news.address">{{ news.data.creator }}</a></b> at block <i>{{ news.block }}</i>
+              </div>
+              <div style="font-size:11px;" v-if="news.data.link">
+                Original content at <b style="font-size:9px"><a :href="news.data.link" target="_blank">{{ news.data.link }}</a></b>
+              </div>
+              <div v-if="counters" class="counters">
+                <div v-for="counter in counters" v-bind:key="counter.uuid">
+                  <div v-if="counter.uuid === news.uuid">
+                    <b><b-icon-arrow-up></b-icon-arrow-up> {{ counter.upvotes }}</b> UPVOTES
+                    <b><b-icon-arrow-down></b-icon-arrow-down> {{ counter.downvotes }}</b> DOWNVOTES
+                  </div>
+                </div>
+              </div>
+              <hr>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="node-badge" v-if="connected">{{ connected }}</div>
+  </div>
+</template>
+
+
+<script>
+var LZUTF8 = require('lzutf8');
+
+export default {
+  name: 'home',
+  mounted : function(){
+    this.checkIdaNodes()
+  },
+  methods: {
+      async checkIdaNodes(){
+        var checknodes = this.scrypta.returnNodes()
+        const app = this
+        for(var i = 0; i < checknodes.length; i++){
+          this.axios.get(checknodes[i] + '/wallet/getinfo').then(
+            check => {
+              if(check.data.blocks !== undefined){
+                if(app.connected === ''){
+                  app.connected = check.config.url.replace('/wallet/getinfo','')
+                  app.axios.post(app.connected + '/read', {
+                    protocol: 'news://'
+                  }).then(async response => {
+                    for(let x in response.data.data){
+                      let nws = response.data.data[x].data
+                      if(nws.signature !== undefined){
+                        let verify = await app.scrypta.verifyMessage(nws.pubkey, nws.signature, nws.message)
+                        if(verify !== false){
+                          response.data.data[x].data = JSON.parse(response.data.data[x].data.message)
+                          response.data.data[x].data.title = LZUTF8.decompress(response.data.data[x].data.title, { inputEncoding: 'Base64' })
+                          response.data.data[x].data.text = LZUTF8.decompress(response.data.data[x].data.compressed, { inputEncoding: 'Base64' })
+                          response.data.data[x].data.tags = LZUTF8.decompress(response.data.data[x].data.tags, { inputEncoding: 'Base64' })
+                          response.data.data[x].data.tags = JSON.parse(response.data.data[x].data.tags)
+                          response.data.data[x].data.guid = LZUTF8.decompress(response.data.data[x].data.guid, { inputEncoding: 'Base64' })
+                          response.data.data[x].data.creator = LZUTF8.decompress(response.data.data[x].data.creator, { inputEncoding: 'Base64' })
+                          response.data.data[x].data.link = LZUTF8.decompress(response.data.data[x].data.link, { inputEncoding: 'Base64' })
+                          app.feed.push(response.data.data[x])
+                        }
+                      }else{
+                        if(nws.title !== undefined){
+                          response.data.data[x].data.title = LZUTF8.decompress(nws.title, { inputEncoding: 'Base64' });
+                          response.data.data[x].data.subtitle = LZUTF8.decompress(nws.subtitle, { inputEncoding: 'Base64' });
+                          response.data.data[x].data.image = LZUTF8.decompress(nws.image, { inputEncoding: 'Base64' });
+                          response.data.data[x].data.text = LZUTF8.decompress(nws.text, { inputEncoding: 'Base64' });
+                          if(nws.tags !== undefined){
+                            response.data.data[x].data.tags = LZUTF8.decompress(nws.tags, { inputEncoding: 'Base64' });
+                            response.data.data[x].data.tags = JSON.parse(response.data.data[x].data.tags)
+                            
+                          }else{
+                            response.data.data[x].data.tags = []
+                          }
+                        }
+                        app.feed.push(response.data.data[x])
+                      }
+                    }
+                    app.isLoading = false
+                    app.readCounters()
+                  })
+                }
+              }
+            }
+          )
+        }
+      },
+      async readCounters(){
+        const app = this
+        for(let y in app.feed){
+          let votes = await app.axios.post(app.connected + '/read',{ protocol: 'news://', refID: app.feed[y].uuid })
+          let upvotes = 0
+          let downvotes = 0
+          for(let x in votes.data.data){
+            if(app.voters[app.feed[y].uuid] === undefined){
+              app.voters[app.feed[y].uuid] = {}
+            }
+            if(app.voters[app.feed[y].uuid][votes.data.data[x].address] === undefined){
+              if(votes.data.data[x].data === 'upvote'){
+                upvotes++
+                app.voters[app.feed[y].uuid][votes.data.data[x].address] = 'upvote'
+              }else if(votes.data.data[x].data === 'downvote'){
+                downvotes++
+                app.voters[app.feed[y].uuid][votes.data.data[x].address] = 'downvote'
+              }
+            }
+          }
+          app.counters.push({
+            'uuid': app.feed[y].uuid,
+            'upvotes': upvotes,
+            'downvotes': downvotes
+          })
+        }
+      }
+  },
+  data () {
+    return {
+      scrypta: window.ScryptaCore,
+      axios: window.axios,
+      nodes: [],
+      connected: '',
+      feed: [],
+      isLoading: true,
+      counters: [],
+      voters: {}
+    }
+  }
+}
+</script>
+
+<style>
+  .node-badge{
+    position:fixed; bottom:-3px; font-size:10px; padding:8px; right:10px; z-index:9999;
+  }
+  .feed img{
+    max-width:100%
+  }
+  .arrow-dx{
+    color:#000;
+    font-size:60px!important;
+    position:absolute;
+    top:-10px;
+    right:0;
+  }
+</style>
